a94c851c2b4ffc0c552e482851e9c94b1316d0da
Burcu uğur <burcuturan737@gmail.com>
2022-12-26 14:44:32 +0300

    Exceptions

---
+++ Workout/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Ekrana kullanıcıdan alınan sayısal bir bilgiyi ekrana yazınız.
+        //Yazarken bilgilendirme mesajlarıyla birlikte olsun.
+
+        int sayi = 0;
+        Console.Write("Lütfen bir sayı giriniz: ");
+        int cevap=int.Parse(Console.ReadLine()); //Parse yerine convert.Tounit32 kullanabiliriz.
+        //Parse metodu ile integera çevriliyor.
+        Console.WriteLine("Girdiğiniz sayı: {0}",cevap);
+        Console.WriteLine("Lütfen programdan çıkmak için bir tuşa basınız... ");
+        Console.ReadKey();
+
+        //ÖDEV: 
+        //Kullanıcıdan kenar bilgilerini alıp, bir dikdörtgenin alanını ve çevresini hesaplayınız.Sonucunu ekrana yazdırınız. 
+        
+
+
+
+
+
+    }
+}
